1863d278bf7c8b39d14d81524bfbc475e9074ad5
junbeomwooo <junbeom2.woo@gmail.com>
2024-11-10 22:59:18 +0100

    10  Nov 2024

--- src/components/fixtures/fixturesOverView.tsx
+++ src/components/fixtures/fixturesOverView.tsx
@@ -1,10 +1,17 @@
 "use client";
 
-import { getFixtures, getH2H, getInjuries } from "@/lib/features/fixtureSlice";
+import {
+  getFixtures,
+  getH2H,
+  getInjuries,
+  getFixtruesByRound,
+} from "@/lib/features/fixtureSlice";
 import { useAppDispatch, useAppSelector } from "@/lib/storeHooks";
 import { useEffect, useState, useRef } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import FixtureHeader from "./header/fixtureHeader";
+
+import Image from "next/image";
 
 // import { fixture } from "../../../public/example";
 
@@ -17,7 +24,7 @@ export default function FixturesOverView({
 }) {
   /** 리덕스 초기화 */
   const dispatch = useAppDispatch();
-  const { fixture, injurie, h2h }: any = useAppSelector(
+  const { fixture, injurie, h2h, fixtureByRound }: any = useAppSelector(
     (state) => state.fixtureSlice
   );
 
@@ -34,6 +41,13 @@ export default function FixturesOverView({
       //     awayID: payload?.teams.away.id,
       //   })
       // );
+      dispatch(
+        getFixtruesByRound({
+          leagueID: payload?.league.id,
+          season: payload?.league.season,
+          round: payload?.league.round,
+        })
+      );
     });
   }, [dispatch, id]);
 
@@ -42,162 +56,229 @@ export default function FixturesOverView({
   const awayStats = fixture?.statistics[1]?.statistics;
 
   // getH2H 데이터를 받아와서 확인하고 원하는 데이트가 맞으면 사용하고 아닐 시 사용하지 않기 그 후에 나머지 렌더링 구현하기
+  console.log(fixture);
+  console.log(fixtureByRound);
 
   return (
-    <>
-      <div className="w-full mt-6 max-xl:w-full border-slate-200 border border-solid bg-white p-7 rounded-xl dark:bg-custom-dark dark:border-0 ">
-        {/* 헤더 */}
-        <FixtureHeader fixture={fixture} locale={locale} />
+    <div className="flex w-full justify-center">
+      {/* 헤더 및 메인 컨텐츠 */}
+      <div className="w-9/12 mr-6">
+        <div className="w-full mt-6 max-xl:w-full border-slate-200 border border-solid bg-white p-7 rounded-xl dark:bg-custom-dark dark:border-0 ">
+          {/* 헤더 */}
+          <FixtureHeader fixture={fixture} locale={locale} />
 
-        {/* 카테고리 */}
-        <div className="flex text-base font-normal mt-10 text-gray-800">
-          {/* Facts, Preview */}
-          {fixture?.statistics.length > 0 ? (
-            <div
-              className="flex flex-col cursor-pointer hover:text-gray-400"
-              onClick={() => setTabPage("factsPreview")}
-            >
-              <h1>Facts</h1>
-              {tabPage === "factsPreview" ? (
-                <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
-              ) : (
-                <></>
-              )}
-            </div>
-          ) : (
-            <div
-              className="flex flex-col cursor-pointer hover:text-gray-400"
-              onClick={() => setTabPage("factsPreview")}
-            >
-              {" "}
-              <h1>Preview</h1>
-              {tabPage === "factsPreview" ? (
-                <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
-              ) : (
-                <></>
-              )}
-            </div>
-          )}
-
-          {/* Events */}
-          {fixture?.events.length > 0 && (
-            <div
-              className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
-              onClick={() => setTabPage("events")}
-            >
-              <h1>Events</h1>
-              {tabPage === "events" ? (
-                <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
-              ) : (
-                <></>
-              )}
-            </div>
-          )}
-
-          {/* Lineups */}
-          {fixture?.lineups.length > 0 && (
-            <div
-              className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
-              onClick={() => setTabPage("lineups")}
-            >
-              <h1>Lineup</h1>
-              {tabPage === "lineups" ? (
-                <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
-              ) : (
-                <></>
-              )}
-            </div>
-          )}
-
-          {/* Players */}
-          {fixture?.players.length > 0 && (
-            <div
-              className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
-              onClick={() => setTabPage("players")}
-            >
-              <h1>Players</h1>
-              {tabPage === "players" ? (
-                <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
-              ) : (
-                <></>
-              )}
-            </div>
-          )}
-
-          {/* Stats */}
-          {fixture?.statistics.length > 0 && (
-            <div
-              className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
-              onClick={() => setTabPage("stats")}
-            >
-              <h1>Stats</h1>
-              {tabPage === "stats" ? (
-                <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
-              ) : (
-                <></>
-              )}
-            </div>
-          )}
-
-          {/* Head to Head */}
-          <div
-            className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
-            onClick={() => setTabPage("hth")}
-          >
-            <h1>Head-to-Head</h1>
-            {tabPage === "hth" ? (
-              <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
+          {/* 카테고리 */}
+          <div className="flex text-base font-normal mt-10 text-gray-800">
+            {/* Facts, Preview */}
+            {fixture?.statistics.length > 0 ? (
+              <div
+                className="flex flex-col cursor-pointer hover:text-gray-400"
+                onClick={() => setTabPage("factsPreview")}
+              >
+                <h1>Facts</h1>
+                {tabPage === "factsPreview" ? (
+                  <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
+                ) : (
+                  <></>
+                )}
+              </div>
             ) : (
-              <></>
+              <div
+                className="flex flex-col cursor-pointer hover:text-gray-400"
+                onClick={() => setTabPage("factsPreview")}
+              >
+                {" "}
+                <h1>Preview</h1>
+                {tabPage === "factsPreview" ? (
+                  <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
+                ) : (
+                  <></>
+                )}
+              </div>
             )}
+
+            {/* Events */}
+            {fixture?.events.length > 0 && (
+              <div
+                className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
+                onClick={() => setTabPage("events")}
+              >
+                <h1>Events</h1>
+                {tabPage === "events" ? (
+                  <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            )}
+
+            {/* Lineups */}
+            {fixture?.lineups.length > 0 && (
+              <div
+                className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
+                onClick={() => setTabPage("lineups")}
+              >
+                <h1>Lineup</h1>
+                {tabPage === "lineups" ? (
+                  <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            )}
+
+            {/* Players */}
+            {fixture?.players.length > 0 && (
+              <div
+                className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
+                onClick={() => setTabPage("players")}
+              >
+                <h1>Players</h1>
+                {tabPage === "players" ? (
+                  <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            )}
+
+            {/* Stats */}
+            {fixture?.statistics.length > 0 && (
+              <div
+                className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
+                onClick={() => setTabPage("stats")}
+              >
+                <h1>Stats</h1>
+                {tabPage === "stats" ? (
+                  <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
+                ) : (
+                  <></>
+                )}
+              </div>
+            )}
+
+            {/* Head to Head */}
+            <div
+              className="flex flex-col cursor-pointer hover:text-gray-400 ml-10"
+              onClick={() => setTabPage("hth")}
+            >
+              <h1>Head-to-Head</h1>
+              {tabPage === "hth" ? (
+                <div className="bg-green-600 w-auto h-1 mt-6 rounded-full"></div>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+
+        {/* 볼점유율및 스탯 구현하기 */}
+        {/* Facts , Preview */}
+        {tabPage === "factsPreview" && (
+          <div className="w-full bg-white mt-4 border border-solid border-slate-200 rounded-xl p-7">
+            {/* 경기가 시작했거나 이미 끝난 경우 */}
+            {fixture?.statistics.length > 0 ? (
+              <div className="flex">
+                <div className="w-full">
+                  {/* Match stats */}
+                  <div className="text-base w-full">
+                    <div className="flex justify-center text-sm font-medium mb-8">
+                      <h1>Match Stats</h1>
+                    </div>
+                    <div className="flex justify-center">
+                      <h2 className="text-xs mb-6">Ball possession</h2>
+                    </div>
+                    {/*Ball possession bar */}
+                    <div className="flex w-full">
+                      <div
+                        style={{
+                          width: homeStats[9]?.value,
+                        }}
+                        className="py-2 mr-1 rounded-l-full text-sm flex items-center text-white bg-red-500"
+                      >
+                        <h1 className="ml-5">{homeStats[9].value}</h1>
+                      </div>
+                      <div
+                        style={{
+                          width: awayStats[9]?.value,
+                        }}
+                        className="py-2 ml-1  rounded-r-full  text-sm flex items-center text-white justify-end bg-blue-500"
+                      >
+                        <h1 className="mr-5">{awayStats[9].value}</h1>
+                      </div>
+                    </div>
+
+                    {/* expected goals */}
+                    <div className="flex w-full justify-between mt-8 text-xs">
+                      <div className="ml-2">
+                        <h1>{fixture?.statistics[0].statistics[16].value}</h1>
+                      </div>
+                      <div>
+                        <h1>Expected Goals</h1>
+                      </div>
+                      <div className="mr-2">
+                        <h1>{fixture?.statistics[1].statistics[16].value}</h1>
+                      </div>
+                    </div>
+
+                    {/* total shots */}
+                    <div className="flex w-full justify-between mt-8 text-xs">
+                      <div className="ml-2">
+                        <h1>{fixture?.statistics[0].statistics[2].value}</h1>
+                      </div>
+                      <div>
+                        <h1>Total Shots</h1>
+                      </div>
+                      <div className="mr-2">
+                        <h1>{fixture?.statistics[1].statistics[2].value}</h1>
+                      </div>
+                    </div>
+
+                    {/* Fouls */}
+                    <div className="flex w-full justify-between mt-8 text-xs">
+                      <div className="ml-2">
+                        <h1>{fixture?.statistics[0].statistics[6].value}</h1>
+                      </div>
+                      <div>
+                        <h1>Fouls</h1>
+                      </div>
+                      <div className="mr-2">
+                        <h1>{fixture?.statistics[1].statistics[6].value}</h1>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              // 경기가 시작하지 않은 경우
+              <div>
+                <h1>No</h1>
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+
+
+          {/* 여기 완성시키기 */}
+      {/* 같은 라운드 경기 보여주기 */}
+      <div className="w-3/12 bg-white border-slate-200 border border-solid rounded-xl mt-6 py-7 px-5">
+        <div className="flex">
+          <div>
+            {/* 리그 이름 및 라운드*/}
+            <h1 className="text-xsm">{fixture?.league.name}</h1>
+            <h2>{fixture?.league.round}</h2>
+          </div>
+          <div>
+          <Image
+            src={fixture?.league.logo}
+            alt={fixture?.league.name || "no league name"}
+            width={30}
+            height={30}
+          />
           </div>
         </div>
       </div>
-
-      {/* 볼점유율및 스탯 구현하기 */}
-      {/* Facts , Preview */}
-      {tabPage === "factsPreview" && (
-        <div className="w-full bg-white mt-4 border border-solid border-slate-200 rounded-xl p-7">
-          {/* 경기가 시작했거나 이미 끝난 경우 */}
-          {fixture?.statistics.length > 0 ? (
-            <div className="flex">
-              <div className="w-full">
-              {/* Match stats */}
-              <div className="text-base w-full">
-                <div className="flex justify-center text-sm font-medium mb-8">
-                <h1>Match Stats</h1>
-                </div>
-                <div className="flex justify-center">
-                <h2 className="text-xs mb-6">Ball possession</h2>
-                </div>
-                {/*Ball possession bar */}
-                <div className="flex w-full">
-                  <div style={{
-                    width: homeStats[9]?.value
-                  }}
-                  className="py-2 mr-2 rounded-l-xl text-sm flex items-center text-white bg-red-500"
-                  >
-                    <h1 className="ml-5">{homeStats[9].value}</h1>
-                  </div>
-                  <div style={{
-                    width: awayStats[9]?.value
-                  }}
-                  className="py-2 ml-2  rounded-r-xl  text-sm flex items-center text-white justify-end bg-blue-500"
-                  >
-                    <h1 className="mr-5">{awayStats[9].value}</h1>
-                  </div>
-                </div>
-              </div>
-              </div>
-            </div>
-          ) : (
-            // 경기가 시작하지 않은 경우
-            <div>
-              <h1>No</h1>
-            </div>
-          )}
-        </div>
-      )}
-    </>
+    </div>
   );
 }
--- src/lib/features/fixtureSlice.tsx
+++ src/lib/features/fixtureSlice.tsx
@@ -3,11 +3,49 @@ import axios, { AxiosError } from "axios";
 
 const url = "https://v3.football.api-sports.io";
 
+// 같은 라운드 정보 가져오기
+export const getFixtruesByRound = createAsyncThunk(
+  "fixtureSlice/getFixtruesByRound",
+  async (
+    {
+      leagueID,
+      season,
+      round,
+    }: { leagueID: number; season: number; round: string },
+    { rejectWithValue }
+  ) => {
+    let result = null;
+
+    try {
+      const response = await axios.get(
+        `${url}/fixtures?league=${leagueID}&season=${season}&round=${round}`,
+        {
+          headers: {
+            "x-rapidapi-host": "v3.football.api-sports.io",
+            "x-rapidapi-key": `${process.env.NEXT_PUBLIC_FOOTBALL_API_KEY}`,
+          },
+        }
+      );
+      result = response.data.response;
+
+    } catch (err) {
+      const axiosErr = err as AxiosError;
+      console.group("getFixtruesByRound Error");
+      result = rejectWithValue(axiosErr.response);
+      console.groupEnd();
+    }
+
+    return result;
+  }
+);
 
 // Head to Head 정보 가져오기
 export const getH2H = createAsyncThunk(
   "fixtureSlice/getH2H",
-  async ({ homeID,awayID }: { homeID: number, awayID: number}, { rejectWithValue }) => {
+  async (
+    { homeID, awayID }: { homeID: number; awayID: number },
+    { rejectWithValue }
+  ) => {
     let result = null;
 
     try {
@@ -22,7 +60,6 @@ export const getH2H = createAsyncThunk(
       );
 
       result = response.data.response;
-      
     } catch (err) {
       const axiosErr = err as AxiosError;
       console.group("getH2H Error");
@@ -34,26 +71,21 @@ export const getH2H = createAsyncThunk(
   }
 );
 
-
 // 해당되는 경기의 부상 정보 가져오기
 export const getInjuries = createAsyncThunk(
   "fixtureSlice/getInjuries",
-  async ({ id }: { id: number}, { rejectWithValue }) => {
+  async ({ id }: { id: number }, { rejectWithValue }) => {
     let result = null;
 
     try {
-      const response = await axios.get(
-        `${url}/injuries?fixture=${id}`,
-        {
-          headers: {
-            "x-rapidapi-host": "v3.football.api-sports.io",
-            "x-rapidapi-key": `${process.env.NEXT_PUBLIC_FOOTBALL_API_KEY}`,
-          },
-        }
-      );
+      const response = await axios.get(`${url}/injuries?fixture=${id}`, {
+        headers: {
+          "x-rapidapi-host": "v3.football.api-sports.io",
+          "x-rapidapi-key": `${process.env.NEXT_PUBLIC_FOOTBALL_API_KEY}`,
+        },
+      });
 
       result = response.data.response;
-      
     } catch (err) {
       const axiosErr = err as AxiosError;
       console.group("getInjuries Error");
@@ -68,22 +100,18 @@ export const getInjuries = createAsyncThunk(
 // 해당되는 경기 정보 가져오기
 export const getFixtures = createAsyncThunk(
   "fixtureSlice/getFixtures",
-  async ({ id }: { id: number}, { rejectWithValue }) => {
+  async ({ id }: { id: number }, { rejectWithValue }) => {
     let result = null;
 
     try {
-      const response = await axios.get(
-        `${url}/fixtures?id=${id}`,
-        {
-          headers: {
-            "x-rapidapi-host": "v3.football.api-sports.io",
-            "x-rapidapi-key": `${process.env.NEXT_PUBLIC_FOOTBALL_API_KEY}`,
-          },
-        }
-      );
+      const response = await axios.get(`${url}/fixtures?id=${id}`, {
+        headers: {
+          "x-rapidapi-host": "v3.football.api-sports.io",
+          "x-rapidapi-key": `${process.env.NEXT_PUBLIC_FOOTBALL_API_KEY}`,
+        },
+      });
 
       result = response.data.response[0];
-      
     } catch (err) {
       const axiosErr = err as AxiosError;
       console.group("getFixtures Error");
@@ -101,6 +129,7 @@ export const fixtureSlice = createSlice({
     fixture: null,
     injurie: null,
     h2h: null,
+    fixtureByRound: null
   },
   reducers: {
     // 현재 상태값 불러오기
@@ -126,10 +155,16 @@ export const fixtureSlice = createSlice({
     builder.addCase(
       getH2H.fulfilled,
       (state, { payload }: { payload: any }) => {
-        console.log(payload);
+        state.h2h = payload;
       }
     );
-    
+
+    builder.addCase(
+      getFixtruesByRound.fulfilled,
+      (state, { payload }: { payload: any }) => {
+        state.fixtureByRound = payload;
+      }
+    );
   },
 });
 
