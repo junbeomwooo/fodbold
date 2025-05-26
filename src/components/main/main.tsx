451004a465f50fc025b3b2d8a195698d62193110
junbeomwooo <junbeom2.woo@gmail.com>
2025-05-26 21:19:21 +0200

    Implemented translations for notification pop up

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -6,6 +6,7 @@ import Standing from "./standing";
 import { TbPlayFootball } from "react-icons/tb";
 
 import { Fragment, useState } from "react";
+import { useTranslations } from "next-intl";
 
 export default function Main({
   standing,
@@ -17,6 +18,7 @@ export default function Main({
   leagueData: any;
 }) {
   const [isPopup, setIsPopup] = useState(true);
+  const m = useTranslations("main");
   return (
     <Fragment>
       <div className="flex w-full h-full px-14 pt-28 dark:bg-black max-lg:block max-msm:px-4 ">
@@ -30,31 +32,31 @@ export default function Main({
             <div className="w-full h-20 bg-[#E2E8F0] dark:bg-[#1D1D1D] flex items-center justify-center rounded-t-xl">
               <TbPlayFootball className="text-black dark:text-white w-[50px] h-[50px]" />
             </div>
-            <hr className="border-white dark:border-[#5F5F5F] border-[0.5px]"/>
+            <hr className="border-white dark:border-[#5F5F5F] border-[0.5px]" />
             {/* Box */}
             <div
               className="bg-white dark:bg-[#1D1D1D] p-8 rounded-b-xl shadow-lg z-60 w-[450px]"
               onClick={(e) => e.stopPropagation()} // 팝업 내부 클릭은 닫히지 않게
             >
-              <h1 className="text-2xl font-bold text-black dark:text-white">Notification</h1>
+              <h1 className="text-2xl font-bold text-black dark:text-white">
+                {m("notification")}
+              </h1>
 
               <p className="text-black dark:text-white text-base mt-8 font-light">
-                This site uses API-Football’s free tier, which is limited to 100
-                requests per day and 10 per minute.
+                {m("notification1")}
               </p>
               <p className="text-black dark:text-white text-base mt-2 font-light">
-                If data fails to load, please try again in 1 minute.
+                {m("notification2")}
               </p>
               <p className="text-black dark:text-white text-base mt-2 font-light">
-                Once the daily limit is reached, the service will resume the
-                following day.
+                {m("notification3")}
               </p>
               <button
                 className="mt-[40px] mb-1 px-4 py-[13px] bg-[#16A348] text-white rounded w-full 
              transform transition-transform duration-200 hover:scale-[103%]"
                 onClick={() => setIsPopup(false)}
               >
-                Got it!
+                {m("gotit")}
               </button>
             </div>
           </div>
